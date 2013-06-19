e4bcc7cc2c6f88afbc189a748e83ae1fecd8c8ea
Marco Galardini <galactus8403@gmail.com>
2013-06-19 11:09:35 +0200

    list also the draft genomes

--- upstream/list_genbank_id.sh
+++ upstream/list_genbank_id.sh
@@ -6,5 +6,7 @@ fi
 genus=$1
 species=$2
 
+echo "Complete genomes"
 curl ftp://ftp.ncbi.nih.gov/genomes/Bacteria/ -l -s | grep  $genus"_"$species
-
+echo "Draft genomes"
+curl ftp://ftp.ncbi.nih.gov/genomes/Bacteria_DRAFT/ -l -s | grep  $genus"_"$species
