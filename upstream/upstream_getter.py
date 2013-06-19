cf942557370e0fd27c1e4bc9984b8ce9f87b1224
Marco Galardini <galactus8403@gmail.com>
2013-06-19 11:05:14 +0200

    correctly get the downstream bases

--- upstream/upstream_getter.py
+++ upstream/upstream_getter.py
@@ -91,35 +91,50 @@ def get_subseq(tupla,genome_seq,flag=False,b_up=400,b_down=0):
                 if len(seq) > b_up+b_down:
                      seq=seq[-b_up:]
                 return seq
+     if b_down > 0:
+        if strand == '+':
+            left=tupla[1]
+            right=tupla[1]+b_down
+            return whole_seq[left:right]
+        else:
+            left=tupla[0]-b_down
+            right=tupla[0]
+            return rev_complement(whole_seq[left:right]) 
      return ''
 
-def get_all_upstreams(genes,whole_seq,b_up=400,b_down=0):
-     tot=len(genes)
-     if tot == 0:
-          return
-     elif tot == 1:
-          if genes[0].strand == '+':
-                upstr = [(genes[0].start-b_up,genes[0].start+b_down,genes[0].name,genes[0].strand)]
-          else:
-                upstr = [(genes[0].end-b_down,genes[0].end+b_up,genes[0].name,genes[0].strand)]
-     else:
-          upstr=[  (genes[i-1].end,genes[i].start,genes[i].name,genes[i].strand) if genes[i].strand == '+'
-                          else 
-                      (genes[i].end,genes[i+1].start,genes[i].name,genes[i].strand)
-                          for i in range(-1,tot-1)]
-          upstr= upstr[1:] + [upstr[0]]
-     print upstr[0][2]
-     out=[Upstream(upstr[0][2],upstr[0][1],upstr[0][0],get_subseq(upstr[0],whole_seq,'wrapped around',b_up,b_down),upstr[0][3])]
-     out+=[Upstream(up[2],up[1],up[0],get_subseq(up,whole_seq,b_up=b_up,b_down=b_down),up[3]) for up in upstr[1:tot - 1]]
-     out+=[Upstream(upstr[-1][2],upstr[-1][1],upstr[-1][0],get_subseq(upstr[-1],whole_seq,'wrapped around',b_up,b_down),upstr[-1][3])]
-     return out
+def get_all_upstreams(genes,whole_seq,b_up=400,b_down=0,is_circ=True):
+    tot=len(genes)
+    if tot == 0:
+        return
+    elif tot == 1:
+        if genes[0].strand == '+':
+            upstr = [(genes[0].start-b_up,genes[0].start+b_down,genes[0].name,genes[0].strand)]
+        else:
+            upstr = [(genes[0].end-b_down,genes[0].end+b_up,genes[0].name,genes[0].strand)]
+    else:
+        upstr=[  (genes[i-1].end,genes[i].start,genes[i].name,genes[i].strand) if genes[i].strand == '+'
+                    else 
+                 (genes[i].end,genes[i+1].start,genes[i].name,genes[i].strand)
+                    for i in range(-1,tot-1)]
+        upstr= upstr[1:] + [upstr[0]]
+    if is_circ:
+        out=[Upstream(upstr[0][2],upstr[0][1],upstr[0][0],get_subseq(upstr[0],whole_seq,'wrapped around',b_up,b_down),upstr[0][3],b_up,b_down)]
+        out+=[Upstream(up[2],up[1],up[0],get_subseq(up,whole_seq,b_up=b_up,b_down=b_down),up[3],b_up,b_down) for up in upstr[1:tot - 1]]
+        out+=[Upstream(upstr[-1][2],upstr[-1][1],upstr[-1][0],get_subseq(upstr[-1],whole_seq,'wrapped around',b_up,b_down),upstr[-1][3],b_up,b_down)]
+    else:
+        out=[Upstream(up[2],up[1],up[0],get_subseq(up,whole_seq,b_up=b_up,b_down=b_down),up[3],b_up,b_down) for up in upstr]
+    return out
 
 def export_upstreams(upstreams,nome_out,infos):
     out=open(nome_out,'w')
     organism,molecule=infos
     for up in upstreams:
-        name,from_,size,start,end,strand=up.infos
-        to_write='>%s; upstream from %s to -1; size: %s; feature type: cds; location:%s:%s:%s:%s:%s\n' %(name,from_,size,organism,molecule,start,end,strand)
+        name,from_,size,start,end,strand,b_up,b_down=up.infos
+        # upstream bases
+        upstream = -(size-b_down)
+        if upstream > 0:
+            upstream=0
+        to_write='>%s; upstream from %s to %s; size: %s; feature type: cds; location:%s:%s:%s:%s:%s\n' %(name,upstream,b_down,size,organism,molecule,start,end,strand)
         seq=nice_write(up.seq)
         out.write(to_write)
         out.write(seq)
@@ -141,19 +156,23 @@ class Gene(object):
     
 class Upstream(object):
     #
-    def __init__(self,name,end,start,seq,strand):
+    def __init__(self,name,end,start,seq,strand,b_up,b_down):
         self.name=name
         self.start=start
         self.end  =end
         self.strand=strand
         self.seq=seq
         self.length=len(self.seq)
+        self.b_up = int(b_up)
+        self.b_down = int(b_down)
         self.infos=[ self.name,
                     -self.length,
                      self.length,
                      self.start,
                      self.end,
-                     self.strand]
+                     self.strand,
+                     -self.b_up,
+                     self.b_down]
                      
     #
 #
@@ -163,7 +182,7 @@ class Upstream(object):
 #             Inputs             #
 ############################
 
-usage='''python upstream_getter.py ORGANISM bases_up bases_down
+usage='''python upstream_getter.py ORGANISM bases_up bases_down IS_CIRCULAR
             if organism name is unknown, try using "list_genbank_id.sh"
 '''
 
@@ -171,9 +190,10 @@ if __name__=='__main__':
     # do stuff
     args=sys.argv
     try:
-        organism, b_up, b_down = args[1:4]
+        organism, b_up, b_down, is_circ = args[1:5]
         b_up = int(b_up)
         b_down = int(b_down)
+        is_circ=bool(is_circ)
     except:
         print usage
         sys.exit()
@@ -194,7 +214,8 @@ if __name__ == '__main__':
 			print 'no genes in', replicon
 			continue
 		whole_seq=get_whole_genome_seq(record)
-		upstreams=get_all_upstreams(genes,whole_seq)
+		upstreams=get_all_upstreams(genes,whole_seq, b_up, b_down, is_circ)
+		if not upstreams:continue
 		file_name='%s_%s_upstreams.fasta' %(organism,replicon)
 		infos=(organism,replicon)
 		export_upstreams(upstreams,file_name,infos)
